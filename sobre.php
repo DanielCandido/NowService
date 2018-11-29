445be928a6d54e2504789711648411f133017e2e
DanielCandido <daniel@musardos.com.br>
2018-11-29 14:47:43 -0200

    news banners

---
+++ sobre.php
@@ -0,0 +1,39 @@
+<?php 
+include_once'head.php'?>
+<?php include_once'header.php' ?>
+<body>
+	<div class="container">
+	<div class="card w-75" style="margin-top: 50px;">
+	  <div class="card-body">
+	    <h5 class="card-title">Quem somos?</h5>
+	    <p class="card-text">A Workers é uma plataforma que atua na divulgação e prestação de serviços.</p>
+	    <p class="card-text">Serviços esses voltados para a manutenção de sua casa ou empresa.</p>
+	  </div>
+	</div>
+
+	<div class="card-group" style="margin-top: 50px;">
+  <div class="card">
+    <img class="card-img-top" src="/img/missao.png" style="height: 250px; " alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Missão</h5>
+      <p class="card-text">Fazer com que as pessoas consigam seus objetivos, é facilitar o dia-a-dia das pessoas através da tecnologia.</p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="/img/visao.png" style="height: 250px;" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Visão</h5>
+      <p class="card-text">Ser referência na areá de prestação de serviços.</p>
+         </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="/img/valores.png" style="height: 250px;" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Valores</h5>
+      <p class="card-text">Compromisso, ética, dedicação, confiabilidade, inovação.</p>
+    </div>
+  </div>
+</div>
+</div>
+</body>
+</html
